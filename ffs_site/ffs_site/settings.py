04c6bfde4a76f73bdf9815d8c184ff4c64b908fd
m0nk3y500 <MsshSZfm0nk3y>
2022-03-31 16:40:15 +0200

    update gitignore // PublicDB erstellt

--- ffs_site/ffs_site/settings.py
+++ ffs_site/ffs_site/settings.py
@@ -79,10 +79,17 @@ WSGI_APPLICATION = 'ffs_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_main.sqlite3',
     }
 }
 
