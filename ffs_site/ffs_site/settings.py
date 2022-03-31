708bdf28dc1047649b6473528749b35d9b679007
m0nk3y500 <MsshSZfm0nk3y>
2022-03-31 18:09:18 +0200

    Login Design überarbeitet

--- ffs_site/ffs_site/settings.py
+++ ffs_site/ffs_site/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db_main.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
